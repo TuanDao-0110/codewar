8308c96fa32d5bd85dd951a0e0dddc0a7d7832d2
tuan <rickytuan1991@gmail.com>
2022-10-31 21:12:21 +0200

    try to do use firebase

---
+++ firebase_services/db_services.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAfuJIg1RK2Q8hVGRo72a4FT_OCGPlXFQ0",
+    authDomain: "my-id-data.firebaseapp.com",
+    projectId: "my-id-data",
+    storageBucket: "my-id-data.appspot.com",
+    messagingSenderId: "702000370548",
+    appId: "1:702000370548:web:96673bbaea655f068c9221",
+    measurementId: "G-BCWFPSVHBN"
+};
+
+
+
+
+module.exports = {firebaseConfig}
--- test.js
+++ test.js
@@ -1,3 +1,20 @@
-const {initializeApp} =require("firebase/app")
-const {getFirestore,collection,getDoc} = require("firebase/firestore/lite")
+const { initializeApp } = require("firebase/app")
+const { getFirestore, collection, getDoc } = require("firebase/firestore/lite")
+const { firebaseConfig } = require('./firebase_services/db_services')
+const { getAnalytics } = require("firebase/analytics");
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+async function getUser(db) {
+    try {
+        const userCol = collection(db, 'user');
+        console.log(userCol)
+        const userSnapShot = await getDoc(userCol);
+        const userList = userSnapShot.docs.map(doc => doc.data());
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getUser(db)
